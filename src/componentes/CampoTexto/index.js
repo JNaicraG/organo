944c07d6c1de8890820a3e41edd41743fe3365d8
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-07-12 15:26:42 -0300

    chore(CampoTexto): implemented onChage/aoDigitar

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,17 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) =>{
     const placeholderModificado = `${props.placeholder}...`
+    let value = '';
+
+    const aoDigitar = (event) =>{
+        value = event.target.value;
+        console.log(value);
+    }
+
     return (
         <div className='campo-texto'>
             <label>{props.label}</label>
-            <input placeholder={placeholderModificado} />
+            <input required={props.obrigatorio} onChange={aoDigitar} placeholder={placeholderModificado} />
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,38 @@
+import { Botao } from '../Botao';
 import { CampoTexto } from '../CampoTexto';
+import { ListaSuspensa } from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+    const times = [
+        'Back-End',
+        'Front-End',
+        'Data-Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ];
+
+    const onSubmit = (event) => {
+        console.log('Form submetido');
+        event.preventDefault(); //Eventos html: https://developer.mozilla.org/en-US/docs/Web/Events -- mas em cammel case no React: onclick vira onClick
+        
+
+
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite sua imagem" />
+                <ListaSuspensa required={true} label="Time" lista={times} />
+                <Botao>
+                    Criar Cards
+                </Botao>
             </form>
         </section>
     )
