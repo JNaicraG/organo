8762e0c409b7c1de1eba04a49d1972cd9b1d4c5d
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-07-12 15:25:41 -0300

    feat(ListaSuspensa): created component

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,9 +1,16 @@
 import './ListaSuspensa.css'
 
-const ListaSuspensa = (props) =>{
+const ListaSuspensa = (props) => {
     return (
-        
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required={props.required}>
+                {
+                    props.lista.map(item => <option key={item}>{item}</option>)
+                }
+            </select>
+        </div>
     )
 }
 
-export {ListaSuspensa}
+export { ListaSuspensa }
