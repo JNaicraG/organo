77d086a943865ccba55094dfa9ec94408f393b4c
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-07-15 08:47:57 -0300

    chore(Formulario,App): implemented useState vars, onSubmit and saveNewColaborador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import { Formulario } from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const onSaveNewColaborador = (colaborador) =>{
+    console.log('colabss: ',colaboradores)
+    console.log('colab: ',colaborador)
+    setColaboradores([...colaboradores, colaborador]);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario onSubmit={colaborador => onSaveNewColaborador(colaborador)} />
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import { ListaSuspensa } from '../ListaSuspensa';
 import { useState } from 'react'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Back-End',
         'Front-End',
@@ -14,19 +14,21 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ];
+    const [nome, setNome] = useState('') //valor inicial ''
+    const [cargo, setCargo] = useState('Instrutor') //valor inicial ''
+    const [imagem, setImagem] = useState('http...') //valor inicial ''
+    const [time, setTime] = useState(times[0]) //valor inicial ''
 
     const onSubmit = (event) => {
         console.log('Form submetido');
         event.preventDefault(); //Eventos html: https://developer.mozilla.org/en-US/docs/Web/Events -- mas em cammel case no React: onclick vira onClick
-
-
-
+        props.onSubmit({ //Novo colaborador obj
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
-
-    const [nome, setNome] = useState('') //valor inicial ''
-    const [cargo, setCargo] = useState('Instrutor') //valor inicial ''
-    const [imagem, setImagem] = useState('http...') //valor inicial ''
-    const [time, setTime] = useState('') //valor inicial ''
 
     return (
         <section className='formulario'>
@@ -39,26 +41,26 @@ const Formulario = () => {
                     value={nome}
                     onChange={value => setNome(value)}
                 />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Cargo" 
-                    placeholder="Digite o seu cargo" 
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite o seu cargo"
                     value={cargo}
                     onChange={value => setCargo(value)}
-                    />
-                <CampoTexto 
-                    label="Imagem" 
-                    placeholder="Digite sua imagem" 
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite sua imagem"
                     value={imagem}
-                    onChange={value => setImagem(value)}
-                    />
-                <ListaSuspensa 
+                    onChange={value => setImagem(value)} //onChange como função no componente
+                />
+                <ListaSuspensa
                     value={time}
-                    onChange={value => setTime(value)}
-                    required={true} 
-                    label="Time" 
-                    lista={times} 
-                    />
+                    required={true}
+                    label="Time"
+                    lista={times}
+                    onChange={value => setTime(value)} //onChange em lambda no componente
+                />
                 <Botao>
                     Criar Cards
                 </Botao>
