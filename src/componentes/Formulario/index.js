9cd274a9c96d7b3ede188d03144da3cc03fee780
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-07-26 09:40:18 -0300

    chore(Formulario): implemented clearing vars after submit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -27,7 +27,11 @@ const Formulario = (props) => {
             cargo,
             imagem,
             time
-        })
+        });
+        setNome('');
+        setCargo('');
+        setImagem('');
+        setTime('');
     }
 
     return (
