3339625137d6a9ec198fe9b96256cf05b3122734
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-07-29 12:33:18 -0300

    feat(App): implemented times and debbuger explained

--- src/App.js
+++ src/App.js
@@ -8,60 +8,63 @@ function App() {
 
   const times = [
     {
-      nome:'Back-End',
-      corPrimaria:'#57C278',
-      corSecundaria:'#D9F7E9',
+      nome: 'Back-End',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
     },
     {
-      nome:'Front-End',
-      corPrimaria:'#82CFFA',
-      corSecundaria:'#E8F8FF',
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
     },
     {
-      nome:'Data-Science',
-      corPrimaria:'#A6D157',
-      corSecundaria:'#F0F8E2',
+      nome: 'Data-Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
     },
     {
-      nome:'DevOps',
-      corPrimaria:'#E06B69',
-      corSecundaria:'#FDE7E8',
+      nome: 'DevOps',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
     },
     {
-      nome:'UX e Design',
-      corPrimaria:'#DB6EBF',
-      corSecundaria:'#FAE9F5',
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5',
     },
     {
-      nome:'Mobile',
-      corPrimaria:'#FFBA05',
-      corSecundaria:'#FFF5D9',
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9',
     },
     {
-      nome:'Inovação e Gestão',
-      corPrimaria:'#FFEEDF',
-      corSecundaria:'#FF8A29',
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FFEEDF',
+      corSecundaria: '#FF8A29',
     }
-];
+  ];
 
   const [colaboradores, setColaboradores] = useState([]);
 
   const onSaveNewColaborador = (colaborador) => {
     console.log('colabss: ', colaboradores)
     console.log('colab: ', colaborador)
+    debugger //Para o fluxo da aplicação para debug
     setColaboradores([...colaboradores, colaborador]);
   }
+
+  console.log('colabs app: ', colaboradores)
 
   return (
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} onSubmit={colaborador => onSaveNewColaborador(colaborador)} />
-      {times.map(time => <Time 
-      nome={time.nome} 
-      corPrimaria={time.corPrimaria} 
-      corSecundaria={time.corSecundaria} 
-      key={time.nome} 
-      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      {times.map(time => <Time
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
+        key={time.nome}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
       <Footer />
     </div>
