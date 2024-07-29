77933017e735be8648bb2661c9928825b8158895
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-07-29 12:30:43 -0300

    chore(ListaSuspensa): implemented blank option

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,6 +5,7 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select required={props.required} value={props.value} onChange={event => props.onChange(event.target.value)}>
+                <option value=""></option>
                 {
                     //props.lista.map(item => <option key={item.nome}>{item.nome}</option>)
                     props.lista.map(item => <option key={item}>{item}</option>)
